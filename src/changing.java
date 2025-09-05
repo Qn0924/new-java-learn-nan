92e7bfd95bddda5578be62f1c219271722ce4f96
Qn0924 <2128904020@qq.com>
2025-09-05 20:43:31 +0800

    变量常量

---
+++ src/changing.java
@@ -0,0 +1,10 @@
+public class changing {
+    public static void main(String[] args) {
+        //变量
+        int num=1;
+        //常量(使用final修饰)
+        final int num1=10;
+        num=18;
+
+    }
+}
