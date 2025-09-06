b7a36ed89b7807d828779328b9fad292cacc2054
Qn0924 <2128904020@qq.com>
2025-09-06 20:24:22 +0800

    foreach循环

---
+++ lesson-2/src/Test.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List list= Arrays.asList(1,2,3,4,5,6,7,8,9);
+        for (Object o : list) {
+            System.out.println(o);
+        }
+        //System.out.println("Hello, World!");
+    }
+}
