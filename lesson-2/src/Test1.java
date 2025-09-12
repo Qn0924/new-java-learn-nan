9eab1298648334ed2a832993bd8878ffa3f7f6a5
Qn0924 <2128904020@qq.com>
2025-09-12 20:16:35 +0800

    使用Arrays类里面的一个静态方法toString()和Object类中的toString()不一样Arrays里面的是将数组拼成一个格式清晰，易于阅读的表示形式

--- lesson-2/src/Test1.java
+++ lesson-2/src/Test1.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
+
 public class Test1 {
     public static void main(String[] args) {
         int []arr=new int[3];
         arr[0]=10;
         arr[1]=20;
         arr[2]=30;
+        //数组转字符串拼出来用数组里面的方法Arrays.toString()--是Arrays类中的一个方法
+        //Arrays是一个用来操作数组的类
+        System.out.println(Arrays.toString(arr));
         for (int zuhe : arr) {
             System.out.println(zuhe);
         }
