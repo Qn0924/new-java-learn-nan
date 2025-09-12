0678e110c05a890da8aa1a7ed5ca7083d54651a7
Qn0924 <2128904020@qq.com>
2025-09-12 20:07:32 +0800

    增强for循环又称foreach循环 增强for循环语法 for(元素类型 局部变量（直接代表数组元素的变量）：要遍历的数组或集合) 与普通for区别在于不需要用索引

---
+++ lesson-2/src/Test1.java
@@ -0,0 +1,11 @@
+public class Test1 {
+    public static void main(String[] args) {
+        int []arr=new int[3];
+        arr[0]=10;
+        arr[1]=20;
+        arr[2]=30;
+        for (int zuhe : arr) {
+            System.out.println(zuhe);
+        }
+    }
+}
