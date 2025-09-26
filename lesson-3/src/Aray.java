093be17f60813ab9568bb4968d8a5c8387006690
Qn0924 <2128904020@qq.com>
2025-09-26 15:19:28 +0800

    数组的常用操作： 冒泡排序

---
+++ lesson-3/src/Aray.java
@@ -0,0 +1,18 @@
+public class Aray {
+    public static void main(String[] args) {
+        int arr[]={73,80,62,93,96,87};
+        //冒泡排序
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length-1-i; j++) {
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
