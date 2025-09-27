aee3e6bbada37998fae1d820b123ba9e2a28b0b1
Qn0924 <2128904020@qq.com>
2025-09-27 21:22:31 +0800

    面向对象-类的基本操作

--- lesson-4/src/Fangfa.java
+++ lesson-4/src/Fangfa.java
@@ -1,5 +1,23 @@
 public class Fangfa {
+    public int test1(int num1,int num2){
+        int num=num1+num2;
+        return num;
+    }
+    public void test2(int num1,int num2){
+        int num=num1+num2;
+    }
+    //任意参数列表
+    public int test3(int... nums){
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Fangfa f=new Fangfa();
+        int x=f.test3(1,2,3);
+        System.out.println(x);
     }
 }
