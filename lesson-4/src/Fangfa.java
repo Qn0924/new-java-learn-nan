457b1da549e7746882bc939877628ee53008929e
Qn0924 <2128904020@qq.com>
2025-09-28 17:54:42 +0800

    类中任意参数方法参数类型改为任意用Object,以及数组

--- lesson-4/src/Fangfa.java
+++ lesson-4/src/Fangfa.java
@@ -14,10 +14,17 @@ public class Fangfa {
         }
         return sum;
     }
-
+    //若是想要把参数类型改为任意的可用object
+    public int test4(Object... nums){
+        int sum=0;
+        return sum;
+    }
     public static void main(String[] args) {
         Fangfa f=new Fangfa();
         int x=f.test3(1,2,3);
         System.out.println(x);
+        System.out.println(f.test4(1,2,3,"字符串",1.2,new Exception()));
     }
+    //同理数组也可未必只存同一种类型，可用Object存不同类型-多态
+    Object arr[]={1,2,3,"er"};
 }
