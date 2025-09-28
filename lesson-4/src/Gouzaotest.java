bb246598662894c2259126643d8dd928c5942f5b
Qn0924 <2128904020@qq.com>
2025-09-28 20:02:18 +0800

    构造方法和创建对象

---
+++ lesson-4/src/Gouzaotest.java
@@ -0,0 +1,9 @@
+public class Gouzaotest {
+    public static void main(String[] args) {
+        User user=new User(2,"zhangsan",22);
+        System.out.println(user);
+        System.out.println(user.id);
+        System.out.println(user.name);
+        System.out.println(user.age);
+    }
+}
---
+++ lesson-4/src/User.java
@@ -0,0 +1,13 @@
+public class User {
+    public int id;
+    public String name;
+    public int age;
+
+    public User(){
+    }
+    public User(int id,String name,int age){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+    }
+}
