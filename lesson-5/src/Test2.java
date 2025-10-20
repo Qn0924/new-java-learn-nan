9f11cc15979284b63a2f4ffbf7434e1c0415c546
Qn0924 <2128904020@qq.com>
2025-10-20 21:01:56 +0800

    static关键字

--- lesson-5/src/Test2.java
+++ lesson-5/src/Test2.java
@@ -1,8 +1,10 @@
 public class Test2 {
     public static void main(String[] args) {
-        User user=new User();
-        user.setId(-1);
-
-        user.show();
+//        User user=new User();
+//        user.setId(-1);
+            Userr.name="zhang";
+        System.out.println(Userr.name);
+            Userr.show();
+        //user.show();
     }
 }
---
+++ lesson-5/src/Userr.java
@@ -0,0 +1,6 @@
+public class Userr {
+    public static String name;
+    public static void show(){
+        System.out.println("这时一个userr类");
+    }
+}
