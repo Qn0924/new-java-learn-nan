ce35ea61cb39fa2e1f05ba8e22d02445c5c7d6f7
Qn0924 <2128904020@qq.com>
2025-10-21 11:35:47 +0800

    静态代码块

---
+++ lesson-5/src/Test3.java
@@ -0,0 +1,11 @@
+public class Test3 {
+    public static void main(String[] args) {
+//        Userrr userrr = new Userrr();
+//        Userrr userrr1 = new Userrr();
+//        Userrr userrr2 = new Userrr();
+//        System.out.println(userrr.num);
+//        System.out.println(userrr1.num);
+//        System.out.println(userrr2.num);
+        System.out.println(1);
+    }
+}
---
+++ lesson-5/src/Userrr.java
@@ -0,0 +1,7 @@
+public class Userrr {
+    public static int num;
+    static{
+        num++;
+        System.out.println("执行静态代码快");
+    }
+}
