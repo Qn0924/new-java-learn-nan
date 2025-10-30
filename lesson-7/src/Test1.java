022f10e5d5e7fddc882c9e4501ade0cb61c649e0
Qn0924 <2128904020@qq.com>
2025-10-30 21:02:01 +0800

    包装类重载构造函数传入String类型的数据

---
+++ lesson-7/src/Test1.java
@@ -0,0 +1,22 @@
+public class Test1 {
+    public static void main(String[] args) {
+        Byte byt=new Byte("1");
+        Short sh=new Short("2");
+        Integer integer=new Integer("3");
+        Long long1=new Long("4");
+        Float f1=new Float("5.5");
+        Double d1=new Double("6.0");
+        Character character=new Character('J');
+        Boolean b1=new Boolean("true");
+        System.out.println(byt);
+        System.out.println(sh);
+        System.out.println(integer);
+        System.out.println(long1);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(character);
+        System.out.println(b1);
+
+
+    }
+}
