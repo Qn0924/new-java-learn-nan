91f44f4295c5fc622c326db68d28aa0c41172b59
Qn0924 <2128904020@qq.com>
2025-10-30 21:27:20 +0800

    拆箱接收String类型

---
+++ lesson-7/src/Test2.java
@@ -0,0 +1,18 @@
+public class Test2 {
+    public static void main(String[] args) {
+        byte b=Byte.parseByte("1");
+        System.out.println(b);
+        short s=Short.parseShort("2");
+        System.out.println(s);
+        int i=Integer.parseInt("3");
+        System.out.println(i);
+        long l=Long.parseLong("4");
+        System.out.println(l);
+        float f=Float.parseFloat("5");
+        System.out.println(f);
+        double d=Double.parseDouble("6.6");
+        System.out.println(d);
+        boolean b1=Boolean.parseBoolean("true");
+        System.out.println(b1);
+    }
+}
