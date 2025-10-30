7ef6105d863c453c8cb611e43b45336056d7a8f9
Qn0924 <2128904020@qq.com>
2025-10-30 20:54:34 +0800

    装箱-包装类

---
+++ lesson-7/src/Test.java
@@ -0,0 +1,30 @@
+public class Test {
+    public static void main(String[] args) {
+        byte b=1;
+        Byte byt=new Byte(b);
+//        b=null;
+//        byt=null;
+        short s=2;
+        Short sh=new Short(s);
+        int i=3;
+        Integer i1=new Integer(i);
+        Long l=4L;
+        Long l1=new Long(l);
+        float f=5;
+        Float f1=new Float(f);
+        double d=6.5;
+        Double d1=new Double(d);
+        char c='a';
+        Character c1=new Character(c);
+        boolean b1=true;
+        Boolean b11=new Boolean(b1);
+        System.out.println(byt);
+        System.out.println(sh);
+        System.out.println(i1);
+        System.out.println(l1);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(c1);
+        System.out.println(b11);
+    }
+}
