eb48401e12a916bcd9fe1144104cedcb03644646
Qn0924 <2128904020@qq.com>
2025-11-10 20:54:15 +0800

    线程初始--但这两个是帮助不混淆线程的概念

---
+++ lesson-9/src/Test4.java
@@ -0,0 +1,7 @@
+public class Test4 {
+    public void test(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println("--------Mytest");
+        }
+    }
+}
---
+++ lesson-9/src/Test5.java
@@ -0,0 +1,9 @@
+public class Test5 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("++++++Test5");
+        }
+        Test4 test4=new Test4();
+        test4.test();
+    }
+}
