eafa4acfcf69683e9e5ff045aa5d5a0c4d1ef899
Qn0924 <2128904020@qq.com>
2025-11-15 17:10:01 +0800

    线程调用--继承方式实现休眠

---
+++ lesson-10/src/MyThread.java
@@ -0,0 +1,16 @@
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            if(i==5){
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            System.out.println("------MyThread");
+        }
+    }
+
+}
