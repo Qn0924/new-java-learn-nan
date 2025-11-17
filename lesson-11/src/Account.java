23fd8de1286f3b5247c7c5bd87a13a63ce2c83e9
Qn0924 <2128904020@qq.com>
2025-11-17 21:03:54 +0800

    线程同步--线程异步出现问题

---
+++ lesson-11/src/Account.java
@@ -0,0 +1,13 @@
+public class Account implements Runnable{
+    private static int num;
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        num++;
+        System.out.println(Thread.currentThread().getName() + "是当前的第" + num + "位访客");
+    }
+}
