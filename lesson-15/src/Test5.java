ed3ea1468cf109f51a60d818ce9446f66c2e57c0
Qn0924 <2128904020@qq.com>
2025-12-09 12:25:54 +0800

    枚举以周为例子

---
+++ lesson-15/src/Test5.java
@@ -0,0 +1,11 @@
+public class Test5 {
+    public static void main(String[] args) {
+        System.out.println(Enu.MONDAY);
+        System.out.println(Enu.THURSDAY);
+        System.out.println(Enu.WEDNESDAY);
+        System.out.println(Enu.THURSDAY);
+        System.out.println(Enu.FRIDAY);
+        System.out.println(Enu.SATURDAY);
+        System.out.println(Enu.SUNDAY);
+    }
+}
