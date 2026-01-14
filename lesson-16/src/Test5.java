cdb7c587e37933b063ca2b8e42816ab8633dc60d
Qn0924 <2128904020@qq.com>
2026-01-14 16:10:47 +0800

    日期date的使用

---
+++ lesson-16/src/Test5.java
@@ -0,0 +1,11 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class Test5 {
+    public static void main(String[] args) {
+        Date date=new Date();
+        System.out.println(date);
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String format=simpleDateFormat.format(date);
+        System.out.println(format);
+    }
+}
